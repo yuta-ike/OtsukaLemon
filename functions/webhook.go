4ae8a0b71826a47654398cda35a67ded0962de64
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2019-12-29 22:10:40 +0900

    add log

--- functions/webhook.go
+++ functions/webhook.go
@@ -11,20 +11,29 @@ import (
 func handler(awsRequest events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	accessToken := "52gP1+MnOGvHWEpjzixawGUIW1icZTs3ULhetUCjv6KIad4WZsrLw5whm2OJ62CmQkEIy3xnYXytBcK4jSR1chcZvuXGdWLxEcruW/ZZ5qYs/lGz3JUCbUKZmKnVQU6ecX97O4OXev+QrBbA9AFWkQdB04t89/1O/w1cDnyilFU="
 	secret := "1aac82b5d4e864f29ab5008997bbb9be"
+
+	log.Printf("handler0")
+
 	bot, err := linebot.New(secret, accessToken)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("handler1")
 
 	event, err := UnmarshalLineRequest([]byte(awsRequest.Body))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Printf("handler2")
+
 	for _, event := range event.Event {
 		replyToken := event.ReplyToken
 		bot.ReplyMessage(replyToken, linebot.NewTextMessage("hello")).Do()
 	}
+
+	log.Printf("handler3")
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
