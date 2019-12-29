3bf48e2da6f8fac8f6949f877d20e770b69413f3
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2019-12-29 22:30:16 +0900

    add error lof

--- functions/webhook.go
+++ functions/webhook.go
@@ -30,7 +30,10 @@ func handler(awsRequest events.APIGatewayProxyRequest) (*events.APIGatewayProxyR
 
 	for _, event := range event.Event {
 		replyToken := event.ReplyToken
-		_, _ = bot.ReplyMessage(replyToken, linebot.NewTextMessage("hello")).Do()
+		_, err = bot.ReplyMessage(replyToken, linebot.NewTextMessage("hello")).Do()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	log.Printf("handler3")
