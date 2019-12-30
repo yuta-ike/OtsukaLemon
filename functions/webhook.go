1e5eca450c90aed6b20efe06b0635f28531318f6
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2019-12-30 13:39:48 +0900

    add image

--- functions/webhook.go
+++ functions/webhook.go
@@ -31,14 +31,16 @@ func handler(awsRequest events.APIGatewayProxyRequest) (*events.APIGatewayProxyR
 		} else {
 			message = "テキストメッセージを送ってね"
 		}
-		if _, err = bot.ReplyMessage(replyToken, linebot.NewImageMessage("https://4.bp.blogspot.com/-ICHHirmVkJQ/Vq89AnGcG_I/AAAAAAAA3kA/iiDZGFKOEiE/s800/fruit_slice09_lemon.png", "https://4.bp.blogspot.com/-ICHHirmVkJQ/Vq89AnGcG_I/AAAAAAAA3kA/iiDZGFKOEiE/s800/fruit_slice09_lemon.png")).Do(); err != nil {
-			log.Fatal(err)
-		}
 
-		log.Print(message)
-		// if _, err = bot.ReplyMessage(replyToken, linebot.NewTextMessage(message)).Do(); err != nil {
-		// 	log.Fatal(err)
-		// }
+		if message == "おつかれもん" {
+			if _, err = bot.ReplyMessage(replyToken, linebot.NewTextMessage(message), linebot.NewImageMessage("https://4.bp.blogspot.com/-ICHHirmVkJQ/Vq89AnGcG_I/AAAAAAAA3kA/iiDZGFKOEiE/s800/fruit_slice09_lemon.png", "https://4.bp.blogspot.com/-ICHHirmVkJQ/Vq89AnGcG_I/AAAAAAAA3kA/iiDZGFKOEiE/s800/fruit_slice09_lemon.png")).Do(); err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			if _, err = bot.ReplyMessage(replyToken, linebot.NewTextMessage(message)).Do(); err != nil {
+				log.Fatal(err)
+			}
+		}
 
 	}
 
